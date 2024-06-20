eda1195962ab1c10649c957efd99822131587414
navod99 <navx99@gmail.com>
2024-06-20 22:47:10 +0530

    fix: update navbar

--- bookbase/src/app/components/Navbar.tsx
+++ bookbase/src/app/components/Navbar.tsx
@@ -5,17 +5,17 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav className='flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5 my-10 pb-4 mx-auto max-w-5xl border-b-2 border-gray-200'>
+        <nav className='flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5 px-10 border-b-2 border-gray-200'>
             <div className="flex items-center justify-center sm:justify-start w-full sm:w-auto">
                 <Image
                     src={Logo}
                     alt='BookBase logo'
-                    width={100}
+                    width={80}
                     quality={100}
                 />
             </div>
-            <Link className='hover:text-black underline sm:no-underline' href="/">Dashboard</Link>
-            <Link className='hover:text-black underline sm:no-underline' href="/tickets">Tickets</Link>
+            <Link className='font-bold hover:text-black underline sm:no-underline' href="/">Home</Link>
+            <Link className='font-bold hover:text-black underline sm:no-underline' href="/addbook">Add Book</Link>
         </nav>
     )
 }
