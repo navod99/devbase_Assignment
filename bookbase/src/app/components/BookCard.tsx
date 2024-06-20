188f2484a81b2f22203a5ac7469b233b115310c0
navod99 <navx99@gmail.com>
2024-06-20 22:48:14 +0530

    feat: create card component

---
+++ bookbase/src/app/components/BookCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BookDetails } from '../types/Book'
+
+interface Props {
+    book: BookDetails
+}
+export default function BookCard({ book }: Props) {
+    return (
+        <div className='max-w-sm rounded overflow-hidden shadow-lg p-6 bg-white'>
+            <div className="font-bold text-xl mb-2">{book.title}</div>
+            <p className="text-gray-700 text-base">
+                {book.author}
+            </p>
+        </div>
+        )
+}
