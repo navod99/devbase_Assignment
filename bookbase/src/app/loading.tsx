ffe88a104beaf47874698fec4b93bd219f10782a
navod99 <navx99@gmail.com>
2024-06-21 02:41:37 +0530

    feat: create loading component

---
+++ bookbase/src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <main className="text-center">
+            <h2 className="font-bold text-primary text-2xl ">Loading...</h2>
+            <p className='font-bold'>Hopefully not for to long :)</p>
+        </main>)
+}
