6ca47f6c8edda2a43f1650e5af84bee847d475fd
navod99 <navx99@gmail.com>
2024-06-21 02:47:04 +0530

    fix: move the get api to booklist component

--- bookbase/src/app/components/BookList.tsx
+++ bookbase/src/app/components/BookList.tsx
@@ -2,14 +2,24 @@ import React from 'react'
 import { BookDetails } from '../types/Book'
 import BookCard from './BookCard'
 
-interface Props {
-    books: BookDetails[]
+async function getBooks() {
+    const res = await fetch('http://localhost:5001/books', {
+        next: {
+            revalidate: 0
+        }
+    })
+
+    if (res.ok) {
+        return res.json();
+    }
 }
 
-export default function BookList({ books }: Props) {
+export default async function BookList() {
+    const books: BookDetails[] | undefined = await getBooks()
+
     return (
         <>
-            {books.length > 0 ? (
+            {books && books.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-2">
                     {books.map((book) => {
                         return (
--- bookbase/src/app/page.tsx
+++ bookbase/src/app/page.tsx
@@ -1,33 +1,21 @@
 import Link from "next/link";
 import BookList from "./components/BookList";
-import { BookDetails } from "./types/Book";
+import { Suspense } from "react";
+import Loading from "./loading";
 
-async function getBooks() {
-  const res = await fetch('http://localhost:5001/books', {
-    next: {
-      revalidate: 0
-    }
-  })
-
-  if(res.ok){
-    let books: BookDetails[] = await new Response(res.body).json();
-    return books
-  }
- 
-}
-
-export default async function Home() {
-  const books: BookDetails[] | undefined = await getBooks()
+export default function Home() {
 
   return (
    <main className="my-8 px-10 sm:px-12">
       <div className="flex justify-between my-8">
         <h2 className="font-bold text-primary text-xl underline">Home</h2>
-        <Link href="/tickets">
+        <Link href="/addbook">
           <button className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Book</button>
         </Link>
       </div>
-      <BookList books={books ?? []}/>
-   </main>
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
+    </main>
   );
 }
